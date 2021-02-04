f3d801ecc4e4edb23a0faca5bca29d2977b0e737
Blair Brown <blair.j.brown@gmail.com>
2021-02-03 23:39:50 -0500

    done

---
+++ src/app/movie-list/movie-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
+
+@Component({
+  selector: 'app-movie-list',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.css']
+})
+export class MovieListComponent implements OnInit {
+  movies: Movie[] = this.movieService.getMovies();
+
+  constructor(public movieService : MovieService) { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/movie.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Movie } from './movie';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  public movies : Movie[] = [{title: "Toy Story", releaseYear: 1995}, {title: 'Forrest Gump', releaseYear: 1994}];
+  constructor() { }
+
+  getMovies() {
+    return this.movies;
+  }
+
+}
